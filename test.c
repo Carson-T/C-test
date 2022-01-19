3f273e74412249f1b52d0f705ce91001a3232496
carsont <tan_biubiu@qq.com>
2022-01-19 19:40:54 +0800

    first commit

---
+++ test.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a = 1;
+    for(a;a<10;a++){
+        printf("aaa\n");
+    }
+    return 0;
+}
---
+++ test1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+printf("bbb");
+return 0;
+}
+
