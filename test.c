a04611c6eb58502fd5b20d5cb081b28c663f2f0d
Carson-T <tan_biubiu@qq.com>
2022-01-23 12:33:03 +0800

    ptr

--- test.c
+++ test.c
@@ -1,8 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int *getRandom(){
+    static int r[10];
+    int i;
+    for(i=0;i<10;i++){
+        r[i] = i;
+    }
+    return r;
+}
+
 int main(){
-    int a = 1;
-    for(a;a<10;a++){
-        printf("aaa\n");
+    int p;
+    int i;
+    p = getRandom();
+    for(i=0;i<10;i++){
+        printf("%d\n",p+i);
     }
     return 0;
 }
--- test1.c
+++ test1.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
+#define MAX 10
 int main(){
-printf("bbb");
-return 0;
+    int a[MAX];
+    for(int i=0;i<MAX;i++){
+        a[i] = i*2;
+    }
+    int* ptr = a;
+    for(;ptr<=&a[MAX-1];ptr++){
+        printf("%d\n",*ptr);
+    }
+    ptr = NULL;
 }
-
