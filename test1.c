a50852bc2265fd3c04ad45d86707a829ea770714
Carson-T <tan_biubiu@qq.com>
2022-02-10 18:50:10 +0800

    2.10

--- test1.c
+++ test1.c
@@ -1,25 +1,12 @@
 #include<stdio.h>
-#include<malloc.h>
-#define MAX 10
-#define AAA char
-void aaa(int **p){
-    int b = 2;
-    *p = &b;
-    printf("%p\n",*p);
-}
-
-struct person{
-    int a;
-}p,q;
+#include<errno.h>
+#include"test.h"
+#define aaa(a,b) printf("%d\n",a+b)
 
 int main(){
-    int a = 1;
-    int *p = &a;
-    printf("%p\n",p);
-    aaa(&p);
-    printf("%p\n",p);
-    char c[100];
-    scanf("%s %d",&c,&a);
-    printf("%s %d",c,a);
+    int a,b = 1;
+    char s[] = "qqq";
+    aaa(a,s);
+    printf("%d",AAA);
     return 0;
 }
